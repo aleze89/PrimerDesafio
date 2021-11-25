297f0b1ba677d24fb565fa2538280662826cef56
aleze89 <68794504+aleze89@users.noreply.github.com>
2021-11-25 19:43:11 -0300

    Agregar archivos de proyecto.

---
+++ Models/Posts.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerDesafio.Models
+{
+    class Posts
+    {
+        public int PostsId { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Content { get; set; }
+        public ICollection<User> Users { get; set; }
+
+    }
+}
